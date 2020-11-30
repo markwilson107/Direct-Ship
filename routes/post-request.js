36ef8a1984a153293069f2c71f341d0de364dd00
Wayne Collins <wayne.c@tcwa.com.au>
2020-11-30 19:06:06 +0800

    Models added

--- routes/post-request.js
+++ routes/post-request.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
   // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
+  app.post("/api/newrequest", function(req, res) {
     db.Request.create(req.body).then(function(dbPostRequest) {
       res.json(dbPostRequest);
     });
