9349efdfd982937edbd09aee5ac98956a1a6b394
Mark Wilson <markwilson107@hotmail.com>
2020-12-03 21:33:44 +0800

    Colour coding or requests and notes done.

--- routes/post-request.js
+++ routes/post-request.js
@@ -1,12 +1,24 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // POST route for saving a new post
-  app.post("/api/newrequest", function(req, res) {
-    db.Request.create(req.body).then(function(dbPostRequest) {
-      console.log(dbPostReques)
+  app.post("/api/newrequest", function (req, res) {
+    db.Request.create(req.body).then(function (dbPostRequest) {
+      console.log(dbPostRequest)
       //res.json(dbPostRequest);
     });
   });
 
+  // UPDATE route for request
+  app.put("/api/updaterequest", function (req, res) {
+      db.Request.update({ notes: req.body.note },
+        {
+          where: {
+            id: req.body.id
+          }
+        })
+        .then(function (reqUpdate) {
+          res.json(reqUpdate);
+        });
+  });
 }
